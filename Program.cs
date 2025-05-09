ae5277ca9f36792686143ea3878b062cadde93d8
Yuki <celsonegreli@gmail.com>
2025-05-09 00:08:24 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace PokeBusca
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new PokeBusca());
+        }
+    }
+}
