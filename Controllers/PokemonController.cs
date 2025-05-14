7a0eda0bc6c7c34b988ce30bbaf8b85c99af38ae
Yuki <celsonegreli@gmail.com>
2025-05-14 00:50:32 -0300

    Add project files.

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,53 @@
+﻿using PokeBusca.Models;
+using System.Text.Json.Nodes;
+
+namespace PokeBusca.Controllers
+{
+    public class PokemonController
+    {
+        public PokemonController() { }
+
+        private static HttpClient clienteHttp = new()
+        {
+            BaseAddress = new Uri("https://pokeapi.co"),
+        };
+
+        public async Task<PokemonModel> BuscarPokemon(string inputPokemon)
+        {
+            try
+            {
+                // Faz a requisição GET
+                HttpResponseMessage resposta = await clienteHttp.GetAsync($"/api/v2/pokemon/{inputPokemon}/");
+                // Lança exceção se falhar
+                resposta.EnsureSuccessStatusCode();
+
+                // Lê o JSON como string
+
+                string json = await resposta.Content.ReadAsStringAsync();
+                JsonNode jsonSeparado = JsonNode.Parse(json);
+                /*
+                var opcoes = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true, // Ignora maiúsculas/minúsculas
+                    };
+
+                PokemonModel Pokemon = JsonSerializer.Deserialize<PokemonModel>(respostaJson, opcoes);
+                */
+                PokemonModel Pokemon = new PokemonModel();
+
+                Pokemon.id = ((int)jsonSeparado["id"]);
+                Pokemon.name = jsonSeparado["name"].ToString();
+                Pokemon.front_default = jsonSeparado["sprites"]?["front_default"]?.ToString();
+                Pokemon.back_default = jsonSeparado["sprites"]?["back_default"]?.ToString();
+                Pokemon.front_shiny = jsonSeparado["sprites"]?["front_shiny"]?.ToString();
+                Pokemon.back_shiny = jsonSeparado["sprites"]?["back_shiny"]?.ToString();
+
+                return Pokemon;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Models/PokemonModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace PokeBusca.Models
+{
+    public class PokemonModel
+    {
+        [DisplayName("Número")]
+        public int id { get; set; }
+
+        [DisplayName("Nome")]
+        public string name { get; set; }
+
+        [DisplayName("Sprite Frontal Padrão")]
+        public string front_default { get; set; }
+
+        [DisplayName("Sprite Traseiro Padrão")]
+        public string back_default { get; set; }
+
+        [DisplayName("Sprite Frontal Shiny")]
+        public string front_shiny { get; set; }
+
+        [DisplayName("Sprite Traseiro Shiny")]
+        public string back_shiny { get; set; }
+    }
+}
